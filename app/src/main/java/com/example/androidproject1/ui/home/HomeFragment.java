616282a7078856e790d1e73e68d058fc1097ee30
Tim Burns <tim.marshall.burns95@gmail.com>
2021-11-18 12:50:36 -0500

    worked on frags and acts

--- app/src/main/java/com/example/androidproject1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/androidproject1/ui/home/HomeFragment.java
@@ -10,10 +10,27 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.androidproject1.R;
+import com.example.androidproject1.WorkoutAdapter;
 import com.example.androidproject1.databinding.FragmentHomeBinding;
+import com.example.androidproject1.models.Workout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    WorkoutAdapter myAdapter;
+    ArrayList<Workout> workouts;
 
     private HomeViewModel homeViewModel;
 private FragmentHomeBinding binding;
@@ -25,6 +42,45 @@ private FragmentHomeBinding binding;
 
     binding = FragmentHomeBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
+/*
+RecyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(new RandomNumListAdapter(1234));
+ */
+
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_workout_list);
+
+        recyclerView = root.findViewById(R.id.workoutList);
+        database = FirebaseDatabase.getInstance().getReference("Workout");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+
+        workouts = new ArrayList<>();
+        myAdapter = new WorkoutAdapter(root.getContext(), workouts);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+
+                    Workout workout = dataSnapshot.getValue(Workout.class);
+                    workouts.add(workout);
+
+
+                }
+                myAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 //        final TextView textView = binding.textHome;
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
