755a74e38ef1eaadc97b3ff9726de38d7a20b566
Tim Burns <tim.marshall.burns95@gmail.com>
2021-11-23 22:03:41 -0500

    working on update - updated fname and lname, need to update on profile side next

--- app/src/main/java/com/example/androidproject1/UpdateProfileActivity.java
+++ app/src/main/java/com/example/androidproject1/UpdateProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidproject1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -10,14 +11,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.androidproject1.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class UpdateProfileActivity extends AppCompatActivity {
 
@@ -60,46 +66,47 @@ public class UpdateProfileActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         String uid = firebaseAuth.getCurrentUser().getUid();
 
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference().child("User");
-        rootRef.child(uid).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                user = dataSnapshot.getValue(User.class);
-                Log.d("myapp-email", user.getEmail());
-                Log.d("myapp-username", user.getUsername());
-                // Do something with the retrieved data or Bruce Wayne
-                //ProfileFname.setText(user.getUsername());
-
-                btn=findViewById(R.id.btnUpdate);
-                btn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-//                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-//                ft.replace(R.id.profile_Frag, new ProfileFragment()).commit();
-                        // TODO:  do all updating before reaching finish
-
-                        // Use user or userdoa??? - new?
-                        //
-                        String firstname = fname.getText().toString();
-                        user.setFname(firstname);
-
-                        finish();
-                    }
-                });
-
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.e("UserListActivity", "Error occured");
-                // Do something about the error
-            }
-        });
-
-
+//        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference().child("User");
+//        rootRef.child(uid).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                user = dataSnapshot.getValue(User.class);
+//                Log.d("myapp-email", user.getEmail());
+//                Log.d("myapp-username", user.getUsername());
+//                // Do something with the retrieved data or Bruce Wayne
+//                //ProfileFname.setText(user.getUsername());
+//
+//                btn=findViewById(R.id.btnUpdate);
+//                btn.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+////                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+////                ft.replace(R.id.profile_Frag, new ProfileFragment()).commit();
+//                        // TODO:  do all updating before reaching finish
+//
+//                        // Use user or userdoa??? - new?
+//                        //
+//                        String firstname = fname.getText().toString();
+//                        user.setFname(firstname);
+//
+//                        finish();
+//                    }
+//                });
+//
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                Log.e("UserListActivity", "Error occured");
+//                // Do something about the error
+//            }
+//        });
 
 
+
+
+        // use this to update
         btn=findViewById(R.id.btnUpdate);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,20 +116,46 @@ public class UpdateProfileActivity extends AppCompatActivity {
                 // TODO:  do all updating before reaching finish
 
                 // Use user or userdoa??? - new?
+                String firstName = fname.getText().toString();
+                String lastName = lname.getText().toString();
 
-
+                updatedata(firstName, lastName);
                 finish();
             }
         });
     }
 
-    public void updateProfile(View view) {
-        // update profile to db here
 
+    private void updatedata(String firstName, String lastName) {
+        firebaseAuth = FirebaseAuth.getInstance();
+        String uid = firebaseAuth.getCurrentUser().getUid();
 
-        // below move back to profile frag
-        Intent intent = new Intent(this,ProfileFragment.class );
+        HashMap User = new HashMap();
+        User.put("firstName", firstName);
+        User.put("lastName", lastName);
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference().child("User");
+        rootRef.child(uid).updateChildren(User).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
 
-        startActivity(intent);
+                if (task.isSuccessful()) {
+
+//                    binding.userName.setText("");
+//                    binding.firstName.setText("");
+//                    binding.lastname.setText("");
+//                    binding.age.setText("");
+//                    Toast.makeText(UpdateData.this,"Successfully Updated",Toast.LENGTH_SHORT).show();
+
+                    finish();
+
+                } else {
+
+                    Toast.makeText(UpdateProfileActivity.this, "Failed to Update", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
     }
 }
