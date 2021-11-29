cbe922a5f24cb07ffac83a62a7e0d8aa840c6545
mishapark <66741910+mishapark@users.noreply.github.com>
2021-11-28 21:41:51 -0500

    done plan workout

---
+++ app/src/main/java/com/example/androidproject1/PlanWorkoutAdapter.java
@@ -0,0 +1,79 @@
+package com.example.androidproject1;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidproject1.models.Plan;
+import com.example.androidproject1.models.ScheduleWorkout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class PlanWorkoutAdapter extends RecyclerView.Adapter<PlanWorkoutAdapter.MyViewHolder> {
+    Context context;
+    String date;
+    ArrayList<Plan> list;
+
+    public PlanWorkoutAdapter(Context context, ArrayList<Plan> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public PlanWorkoutAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.plan_item, parent,false);
+        PlanWorkoutAdapter.MyViewHolder vh = new PlanWorkoutAdapter.MyViewHolder(v);
+        context = parent.getContext();
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlanWorkoutAdapter.MyViewHolder holder, int position) {
+        Plan plan = list.get(position);
+        holder.planName.setText(plan.getName());
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference("Schedule1");
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        String uid = firebaseAuth.getCurrentUser().getUid();
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("Delete Workout")
+                        .setMessage("Do you want to delete this workout?")
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                DatabaseReference s = database.child(uid).child(String.valueOf(plan.getId()));
+                                s.removeValue();
+                            }})
+                        .setNegativeButton(android.R.string.no, null).show();
+            }
+        });
+    }
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView planName;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            planName = itemView.findViewById(R.id.planName);
+        }
+    }
+}
--- app/src/main/java/com/example/androidproject1/PlanWorkoutFragment.java
+++ app/src/main/java/com/example/androidproject1/PlanWorkoutFragment.java
@@ -7,30 +7,94 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.androidproject1.models.Plan;
+import com.example.androidproject1.models.ScheduleWorkout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class PlanWorkoutFragment extends Fragment {
+    RecyclerView recycler;
+    DatabaseReference database;
+    PlanWorkoutAdapter myAdapter;
+    ArrayList<Plan> plansList;
+    Date today = new Date();
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM");
+    SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+    TextView month;
+    TextView day;
+    TextView year;
 
-    private PlanWorkoutViewModel mViewModel;
-
-    public static PlanWorkoutFragment newInstance() {
-        return new PlanWorkoutFragment();
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.plan_workout_fragment, container, false);
-    }
+        View view = inflater.inflate(R.layout.plan_workout_fragment, container, false);
+        month = view.findViewById(R.id.month);
+        day = view.findViewById(R.id.day);
+        year = view.findViewById(R.id.year);
+        month.setText(monthFormat.format(today));
+        day.setText(dayFormat.format(today));
+        year.setText(yearFormat.format(today));
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(PlanWorkoutViewModel.class);
-        // TODO: Use the ViewModel
+        recycler = (RecyclerView)view.findViewById(R.id.recycler);
+        database = FirebaseDatabase.getInstance().getReference("Schedule1");
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        String uid = firebaseAuth.getCurrentUser().getUid();
+
+        plansList = new ArrayList<>();
+        myAdapter = new PlanWorkoutAdapter(getActivity(), plansList);
+        recycler.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                plansList.clear();
+                for (DataSnapshot plans : snapshot.getChildren()){
+                    if (plans.getKey().equals(uid)) {
+                        for (DataSnapshot dsWorkout : plans.getChildren()) {
+                            String id = dsWorkout.getKey();
+                            String workout = dsWorkout.child("workout").getValue().toString();
+                            String date = dsWorkout.child("date").getValue().toString();
+
+                            if (sdf.format(today).equals(date)) {
+                                Plan plan = new Plan(workout, id);
+                                plansList.add(plan);
+                            }
+                        }
+                    }
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
     }
 
 }
