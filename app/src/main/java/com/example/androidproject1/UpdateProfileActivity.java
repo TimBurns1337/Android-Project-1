bbfd14eeeb8653eea0968b655fd33e15cf4dd7b4
Tim Burns <tim.marshall.burns95@gmail.com>
2021-11-29 20:14:46 -0500

    working on uploading pics

--- app/src/main/java/com/example/androidproject1/UpdateProfileActivity.java
+++ app/src/main/java/com/example/androidproject1/UpdateProfileActivity.java
@@ -42,21 +42,35 @@ public class UpdateProfileActivity extends AppCompatActivity {
 
     EditText fname, lname, dob, sex, weight, height;
     String FName, LName, DOB, Sex, Weight, Height;
+
+    // vars for posting image to firebase
     private Uri mImageUri = null;
-    private Uri filePath;
-    private static final int PICK_IMAGE_REQUEST = 22;
-    private static final int REQUEST_IMAGE_CAPTURE = 111;
-    private static final int PHOTO_REQUEST = 1200 ;
+    private static final  int GALLERY_REQUEST =1;
+    private static final int CAMERA_REQUEST_CODE=1;
+
+    private ProgressDialog mProgress;
+
+//    private Uri filePath;
+//    private static final int PICK_IMAGE_REQUEST = 22;
+//    private static final int REQUEST_IMAGE_CAPTURE = 111;
+//    private static final int PHOTO_REQUEST = 1200 ;
 
     private User user;
     private FirebaseAuth firebaseAuth;
     FirebaseStorage storage;
     StorageReference storageReference;
+    private DatabaseReference mDatabase;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+        String uid = firebaseAuth.getCurrentUser().getUid();
 
         fname = findViewById(R.id.et_fname);
         lname = findViewById(R.id.et_lname);
@@ -81,60 +95,12 @@ public class UpdateProfileActivity extends AppCompatActivity {
         weight.setText(Weight);
         height.setText(Height);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        String uid = firebaseAuth.getCurrentUser().getUid();
-
-//        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference().child("User");
-//        rootRef.child(uid).addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                user = dataSnapshot.getValue(User.class);
-//                Log.d("myapp-email", user.getEmail());
-//                Log.d("myapp-username", user.getUsername());
-//                // Do something with the retrieved data or Bruce Wayne
-//                //ProfileFname.setText(user.getUsername());
-//
-//                btn=findViewById(R.id.btnUpdate);
-//                btn.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-////                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-////                ft.replace(R.id.profile_Frag, new ProfileFragment()).commit();
-//                        // TODO:  do all updating before reaching finish
-//
-//                        // Use user or userdoa??? - new?
-//                        //
-//                        String firstname = fname.getText().toString();
-//                        user.setFname(firstname);
-//
-//                        finish();
-//                    }
-//                });
-//
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                Log.e("UserListActivity", "Error occured");
-//                // Do something about the error
-//            }
-//        });
-
-
-
-
-        // use this to update
         btn=findViewById(R.id.btnUpdate);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-//                ft.replace(R.id.profile_Frag, new ProfileFragment()).commit();
                 // TODO:  do all updating before reaching finish
 
-
-                // Use user or userdoa??? - new?
                 String firstName = fname.getText().toString();
                 String lastName = lname.getText().toString();
                 String DOB  = dob.getText().toString();
@@ -168,149 +134,47 @@ public class UpdateProfileActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task task) {
 
                 if (task.isSuccessful()) {
-
-//                    binding.userName.setText("");
-//                    binding.firstName.setText("");
-//                    binding.lastname.setText("");
-//                    binding.age.setText("");
-//                    Toast.makeText(UpdateData.this,"Successfully Updated",Toast.LENGTH_SHORT).show();
-                    //uploadImage();
                     finish();
-
                 } else {
-
                     Toast.makeText(UpdateProfileActivity.this, "Failed to Update", Toast.LENGTH_SHORT).show();
-
                 }
-
             }
         });
-
     }
 
     public void addPhoto(View view) {
-        Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //To save to file: photoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.withAppendedPath(locationForPhotos, targetFilename));
-        if (photoIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(photoIntent,PHOTO_REQUEST);
-            //startActivity(photoIntent);
-            photoIntent.setType("image/*");
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
         }
     }
 
-    public void takePhoto(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    public void uploadPic(View view) {
+        startPosting();
+    }
+
+    private void startPosting(){
+
+        mProgress.setMessage("Posting to database...");
+
+        if(mImageUri != null){
+
+            mProgress.show();
+            StorageReference filepath = storageReference.child("Profile_Pic").child(mImageUri.getLastPathSegment());
+
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //Uri downloadUrl =taskSnapshot.getDownloadUrl();
+                    String downloadUrl =taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+
+                    DatabaseReference newPost = mDatabase.push();
+                    newPost.child("image").setValue(downloadUrl);
+
+
+                    mProgress.dismiss();
+                }
+            });
         }
-    }
-    }
-
-
-
-    public void selectImage(View view) {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(
-                Intent.createChooser(
-                        intent,
-                        "Select Image from here..."),
-                PICK_IMAGE_REQUEST);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data)
-    {
-
-        super.onActivityResult(requestCode,
-                resultCode,
-                data);
-
-        // checking request code and result code
-        // if request code is PICK_IMAGE_REQUEST and
-        // resultCode is RESULT_OK
-        // then set image in the image view
-        if (requestCode == PICK_IMAGE_REQUEST
-                && resultCode == RESULT_OK
-                && data != null
-                && data.getData() != null) {
-
-            // Get the Uri of data
-            filePath = data.getData();
-        }
-    }
-
-    // method for uploading picture - work in progress - crashes app
-    private void uploadImage() {
-        if (filePath != null) {
-
-            // Code for showing progressDialog while uploading
-            ProgressDialog progressDialog
-                    = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading...");
-            progressDialog.show();
-
-            // Defining the child of storageReference
-            StorageReference ref
-                    = storageReference
-                    .child(
-                            "images/"
-                                    + UUID.randomUUID().toString());
-
-            // adding listeners on upload
-            // or failure of image
-            ref.putFile(filePath)
-                    .addOnSuccessListener(
-                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
-
-                                @Override
-                                public void onSuccess(
-                                        UploadTask.TaskSnapshot taskSnapshot) {
-
-                                    // Image uploaded successfully
-                                    // Dismiss dialog
-                                    progressDialog.dismiss();
-                                    Toast
-                                            .makeText(UpdateProfileActivity.this,
-                                                    "Image Uploaded!!",
-                                                    Toast.LENGTH_SHORT)
-                                            .show();
-                                }
-                            })
-
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-
-                            // Error, Image not uploaded
-                            progressDialog.dismiss();
-                            Toast
-                                    .makeText(UpdateProfileActivity.this,
-                                            "Failed " + e.getMessage(),
-                                            Toast.LENGTH_SHORT)
-                                    .show();
-                        }
-                    })
-                    .addOnProgressListener(
-                            new OnProgressListener<UploadTask.TaskSnapshot>() {
-
-                                // Progress Listener for loading
-                                // percentage on the dialog box
-                                @Override
-                                public void onProgress(
-                                        UploadTask.TaskSnapshot taskSnapshot) {
-                                    double progress
-                                            = (100.0
-                                            * taskSnapshot.getBytesTransferred()
-                                            / taskSnapshot.getTotalByteCount());
-                                    progressDialog.setMessage(
-                                            "Uploaded "
-                                                    + (int) progress + "%");
-                                }
-                            });
-        }
-
     }
 }
